3afc0ab2eece3eff73be5b6bf3e90894525b92b4
源 <18802956884@163.com>
2016-06-10 21:04:24 +0800

    yuan

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <stdlib.h>
+#include "Print1ToMaxOfNDigits.h"
+using namespace std;
+
+void TestPrint1ToMaxOfNDigits()
+{
+	//Print1ToMaxOfNDigits(1);
+	//Print1ToMaxOfNDigits(3);
+	Print1ToMaxOfNDigits(7);
+}
+
+int main()
+{
+	TestPrint1ToMaxOfNDigits();
+	system("pause");
+	return 0;
+}
